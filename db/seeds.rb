f3399a8e86293a837b0db5ead8d1644c4edbbdae
minako-y <m.tp2uk@gmail.com>
2021-07-16 13:26:20 +0000

    seed作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,47 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+3.times do |n|
+  Customer.create(
+    email: "test#{n + 1}@test",
+    password: "test#{n + 1}@test",
+    last_name: "山田",
+    first_name: "太郎#{n + 1}",
+    kana_last_name: "ヤマダ",
+    kana_first_name: "タロウ#{n + 1}",
+    postal_code: "15000",
+    phone_number: "0000000000#{n + 1}",
+    address: "東京都渋谷区神南1丁目19-11 パークウェースクエア24階",
+    is_deleted: "false"
+  )
+end
+
+Customer.all.each do |customer|
+  customer.destinations.create(
+    postal_code: "1600022",
+    place: "東京都新宿区新宿2-5-10成信ビル 4階",
+    name: "test"
+  )
+end
+
+Genre.create(name: "ケーキ")
+
+4.times do |n|
+  Commodity.create(
+    genre_id: 1,
+    name: "ケーキ#{n + 1}",
+    opinion: "とてもおいしいケーキ#{n + 1}",
+    non_taxed_price: 2500,
+    image: File.open("./app/assets/images/david-holifield-kPxsqUGneXQ-unsplash.jpg"),
+    sale_status: 0
+  )
+end
+
+2.times do |n|
+  CartItem.create(
+    customer_id: 1,
+    commodity_id: "#{n + 1}",
+    quantity: 2
+  )
+end
