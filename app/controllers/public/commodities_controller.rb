c5ec4e0ca113222d936e929161702ab82111ecad
yuko-kochi <happy.k165131@gmail.com>
2021-07-18 07:59:54 +0000

    index

--- app/controllers/public/commodities_controller.rb
+++ app/controllers/public/commodities_controller.rb
@@ -1,9 +1,11 @@
 class Public::CommoditiesController < ApplicationController
-  
+
   def index
+    @commodities = Commodity.all
   end
 
   def show
+    @commodity = Commodity.find(params[:id])
   end
-  
+
 end
