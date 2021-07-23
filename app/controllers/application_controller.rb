6268b5fd27d3293a374d6064422aa5596a596920
ShimonakaRina <sixseasonrain11614@gmail.com>
2021-07-23 08:49:21 +0000

    layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,10 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  def after_sign_up_path_for(resource)
+    customer_path(resource)
+  end
+
   def after_sign_in_path_for(resource)
     case resource
         when Customer
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,26 +1,26 @@
 class Customer < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         
+
   has_many :orders
   has_many :cart_items
   has_many :destinations
   enum is_deleted: { 有効: false, 退会: true }
-  
-  
+
+
   def name
     last_name + first_name
   end
-  
+
   def kana_name
     kana_last_name + kana_first_name
   end
-  
+
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :kana_first_name, presence: true
   validates :kana_last_name, presence: true
-  validates :postal_code, presence: true
+  validates :postal_code, presence: true, format: {with: /\A\d{7}\z/ }
   validates :address, presence: true
-  validates :phone_number, presence: true
+  validates :phone_number, presence: true, format: {with: /\A\d{10,11}\z/ }
 end
