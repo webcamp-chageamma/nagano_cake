98df4c12de53a2fe84ec737607118330f30cf740
ShimonakaRina <sixseasonrain11614@gmail.com>
2021-07-23 08:52:35 +0000

    shusei

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  def after_sign_up_path_for(resource)
-    customer_path(resource)
-  end
-
   def after_sign_in_path_for(resource)
     case resource
         when Customer
