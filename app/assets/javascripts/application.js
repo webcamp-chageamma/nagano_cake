55df51fa3d46780ebcabd9ae73915cd6d2f99f57
yuko-kochi <happy.k165131@gmail.com>
2021-07-25 11:35:44 +0000

    レスポンシブ対応

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,3 +18,13 @@
 //= require activestorage
 //= require turbolinks
 //= require_tree .
+//= require jquery
+
+$(function() {
+  $('#back a').on('click',function(event){
+    $('body, html').animate({
+      scrollTop:0
+    }, 800);
+    event.preventDefault();
+  });
+});
