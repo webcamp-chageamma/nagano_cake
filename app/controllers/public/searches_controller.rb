f2fbed6c1959c7944ed3f898ef1b3d0ad3dc3613
yuko-kochi <happy.k165131@gmail.com>
2021-07-27 09:21:10 +0000

    2

--- app/controllers/public/searches_controller.rb
+++ app/controllers/public/searches_controller.rb
@@ -1,16 +1,16 @@
 class Public::SearchesController < ApplicationController
   def search
-    if params[:content].nil? 
+    if params[:content].nil?
       @value = params["search"]["value"]
       @commodities = Commodity.search_genre_for(@value).page(params[:page]).per(8)
       @genres = Genre.all
       @commodity_count = Commodity.search_genre_for(@value).count
-    else 
+    else
       @content = params[:content]
   	  @commodities = Commodity.search_for(@content).page(params[:page]).per(8)
   	  @genres = Genre.all
-  	  @commodity_count = Commodity.search_genre_for(@value).count
+  	  @commodity_count = Commodity.search_for(@content).count
     end
   end
-  
+
 end
